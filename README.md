5acc2a8d459e6a9048a830ef9b1be368b9663198
Genevieve DuPuy <genevieve.e.dupuy@gmail.com>
2023-05-13 22:39:26 -0700

    attempted table although not fully working. added to README and CSS

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ This a website with information about Pat's Salmon Cookies!
 
 ### Log of Updates
 
+5/10/2023 created files, added images, added hourly cookie calculation
+5/11/2023 added sales display function, name and array to location object, css styling and linked index to css, merged class06-objects branch
+5/13/2023 created new branch class 07, added constructors for objects and utilized prototypes for functions, attempted creating table with displays unsuccessfully, waiting to merge branch
+
 ### Lighthouse Accessibility Report Score
 
 ![Screenshot of Lighthouse Accessibility Report Score]()
--- js/app.js
+++ js/app.js
@@ -2,45 +2,6 @@
 
 const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
-// const seattle = {
-//   name: 'Seattle',
-//   minCust: 23,
-//   maxCust: 65,
-//   avgCookieSale: 6.3,
-//   cookiesSoldPerHr: [],
-// };
-
-// const tokyo = {
-//   name: 'Tokyo',
-//   minCust: 3,
-//   maxCust: 24,
-//   avgCookieSale: 1.2,
-//   cookiesSoldPerHr: [],
-// };
-
-// const dubai = {
-//   name: 'Dubai',
-//   minCust: 11,
-//   maxCust: 38,
-//   avgCookieSale: 3.7,
-//   cookiesSoldPerHr: [],
-// };
-
-// const paris = {
-//   name: 'Paris',
-//   minCust: 20,
-//   maxCust: 38,
-//   avgCookieSale: 2.3,
-//   cookiesSoldPerHr: [],
-// };
-
-// const lima = {
-//   name: 'Lima',
-//   minCust: 2,
-//   maxCust: 16,
-//   avgCookieSale: 4.6,
-//   cookiesSoldPerHr: [],
-// };
 
 function Locations(name, minCust, maxCust, avgCookieSale) {
   this.name = name;
@@ -48,25 +9,16 @@ function Locations(name, minCust, maxCust, avgCookieSale) {
   this.maxCust = maxCust;
   this.avgCookieSale = avgCookieSale;
   this.cookiesSoldPerHr = [];
+  this.totalAllLocations = [];
 }
 
 
-//to figure random customers per hour
-// function randomCustPerHr(minCust, maxCust) {
-//   return Math.floor(Math.random() * (maxCust - minCust) + minCust);
-// }
 
 Locations.prototype.randomCustPerHr = function() {
   return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
 };
 
-//to use random customer per hour figure * average cookie sale to get cookies sold per hour estimate & add each hour to array in object
-// function calculateHourlyCookies(location) {
-//   for (let i = 0; i < hours.length; i++) {
-//     let cookiesSold = Math.ceil(randomCustPerHr(location.minCust, location.maxCust) * location.avgCookieSale);
-//     location.cookiesSoldPerHr.push(cookiesSold);
-//   }
-// }
+
 
 Locations.prototype.calculateHourlyCookies = function() {
   for (let i = 0; i < hours.length; i++) {
@@ -78,78 +30,65 @@ Locations.prototype.calculateHourlyCookies = function() {
   }
 };
 
-//made section in sales.html with id to access it by
-const salesNumbersContainer = document.getElementById('store-sales');
+
+
+const salesNumbersContainer = document.getElementById('storeSales');
+
 
 
 Locations.prototype.displaySalesNumbers = function() {
-  this.calculateHourlyCookies;
+  console.log(this.displaySalesNumbers);
+  this.calculateHourlyCookies();
 
-  const article = document.createElement('article');
-  salesNumbersContainer.appendChild(article);
+  // const cookieTable = document.createElement('table');
+  // salesNumbersContainer.appendChild(cookieTable);
 
-  const heading = document.createElement('h3');
-  heading.textContent = this.name;
-  article.appendChild(heading);
+  // const headerRow = document.createElement('tr');
+  // cookieTable.appendChild(headerRow);
+  // for (let i = 0; i < hours.length; i++) {
+  //   headerRow.textContent = hours[i];
+  // }
+  // let cookieTable = document.querySelector('table');
+  // salesNumbersContainer.append(cookieTable);
 
-  const list = document.createElement('ul');
-  article.appendChild(list);
+  // let tableHead = document.createElement('tr');
+  // let tableHours = document.createElement('th');
+  // tableHead.appendChild(tableHours);
+  // tableHours.textContent = 'hours';
 
-  let totalCookies = 0;
+  let tableRow = document.createElement('tr');
+  salesNumbersContainer.append(tableRow);
+
+  const store = document.createElement('td');
+  tableRow.appendChild(store);
+  store.textContent = this.name;
+
+  const hourlySales = document.createElement('td');
+  tableRow.appendChild(hourlySales);
+  for(let i = 0; i < this.cookiesSoldPerHr.length; i++) {
+    hourlySales.textContent = this.cookiesSoldPerHr;
+  }
+
+
+  let totalDailyCookies = 0;
 
   for (let i = 0; i < hours.length; i++) {
-    totalCookies = this.cookiesSoldPerHr[i] + totalCookies;
-
-    const listItem = document.createElement('li');
-    list.appendChild(listItem);
-    listItem.textContent = `${hours[i]}: ${this.cookiesSoldPerHr[i]} cookies`;
+    totalDailyCookies = this.cookiesSoldPerHr[i] + totalDailyCookies;
   }
-  const totalDailyCookies = document.createElement('li');
-  list.appendChild(totalDailyCookies);
-  totalDailyCookies.textContent = `Total: ${totalCookies} cookies`;
+  const totalAllStoresPerHr = document.createElement('td');
+  tableRow.appendChild(totalAllStoresPerHr);
+  totalAllStoresPerHr.textContent = this.totalAllLocations;
 };
 
 
-//to display the cookie sales numbers for each location with a heading and bulleted hourly sales list ending with a cookies sold total for the day
-// function displaySalesNumbers(location) {
-//   calculateHourlyCookies(location);
-
-//   const article = document.createElement('article');
-//   salesNumbersContainer.appendChild(article);
-
-//   const heading = document.createElement('h3');
-//   heading.textContent = location.name;
-//   article.appendChild(heading);
-
-//   const list = document.createElement('ul');
-//   article.appendChild(list);
-
-//   let totalCookies = 0;
-
-//   for (let i = 0; i < hours.length; i++) {
-//     totalCookies = location.cookiesSoldPerHr[i] + totalCookies;
-
-//     const listItem = document.createElement('li');
-//     list.appendChild(listItem);
-//     listItem.textContent = `${hours[i]}: ${location.cookiesSoldPerHr[i]} cookies`;
-//   }
-//   const totalDailyCookies = document.createElement('li');
-//   list.appendChild(totalDailyCookies);
-//   totalDailyCookies.textContent = `Total: ${totalCookies} cookies`;
-// }
-
-// const totalAllLocations = [seattle, tokyo, dubai, paris, lima];
-
-// for(let i = 0; i < totalAllLocations.length; i++) {
-//   displaySalesNumbers(totalAllLocations[i]);
-// }
 
 function totalAllLocations() {
 
   for(let i = 0; i < totalAllLocations.length; i++) {
-    this.displaySalesNumbers(totalAllLocations[i]);
+    this.displaySalesNumbers.push(totalAllLocations[i]);
   }
 }
+
 
 let seattle = new Locations('Seattle', 23, 65, 6.3);
 let tokyo = new Locations('Tokyo', 3, 24, 1.2);
@@ -164,3 +103,7 @@ console.log(paris);
 console.log(lima);
 
 totalAllLocations();
+
+// get functions for hourly cookies and daily location totals into tr/td's
+// get nested functions for getting totals of ALL locations EACH hour combined along bottom
+
