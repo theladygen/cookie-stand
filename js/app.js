44b38c367a2ba1f9d2b932fdf391fa68e3fd4461
Genevieve DuPuy <genevieve.e.dupuy@gmail.com>
2023-05-15 20:04:27 -0700

    fixed render function and got it to populate table with location totals

--- js/app.js
+++ js/app.js
@@ -3,24 +3,23 @@
 const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
 
-function Locations(name, minCust, maxCust, avgCookieSale) {
+function Location(name, minCust, maxCust, avgCookieSale) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookieSale = avgCookieSale;
   this.cookiesSoldPerHr = [];
-  this.totalAllLocations = [];
 }
 
 
 
-Locations.prototype.randomCustPerHr = function() {
+Location.prototype.randomCustPerHr = function() {
   return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
 };
 
 
 
-Locations.prototype.calculateHourlyCookies = function() {
+Location.prototype.calculateHourlyCookies = function() {
   for (let i = 0; i < hours.length; i++) {
     let custPerHr = this.randomCustPerHr();
     console.log(custPerHr);
@@ -32,55 +31,48 @@ Locations.prototype.calculateHourlyCookies = function() {
 
 
 
-const salesNumbersContainer = document.getElementById('storeSales');
+const salesTable = document.getElementById('sales-table');
 
 
-
-Locations.prototype.displaySalesNumbers = function() {
-  console.log(this.displaySalesNumbers);
+//this render function creates a table row, fills it with data, and adds it to the sales table
+Location.prototype.render = function() {
   this.calculateHourlyCookies();
 
-  // const cookieTable = document.createElement('table');
-  // salesNumbersContainer.appendChild(cookieTable);
-
-  // const headerRow = document.createElement('tr');
-  // cookieTable.appendChild(headerRow);
-  // for (let i = 0; i < hours.length; i++) {
-  //   headerRow.textContent = hours[i];
-  // }
-  // let cookieTable = document.querySelector('table');
-  // salesNumbersContainer.append(cookieTable);
-
-  // let tableHead = document.createElement('tr');
-  // let tableHours = document.createElement('th');
-  // tableHead.appendChild(tableHours);
-  // tableHours.textContent = 'hours';
-
   let tableRow = document.createElement('tr');
-  salesNumbersContainer.append(tableRow);
+  salesTable.append(tableRow);
 
   const store = document.createElement('td');
   tableRow.appendChild(store);
   store.textContent = this.name;
 
-  const hourlySales = document.createElement('td');
-  tableRow.appendChild(hourlySales);
   for(let i = 0; i < this.cookiesSoldPerHr.length; i++) {
-    hourlySales.textContent = this.cookiesSoldPerHr;
+    const hourlySales = document.createElement('td');
+    tableRow.appendChild(hourlySales);
+    hourlySales.textContent = this.cookiesSoldPerHr[i];
   }
 
 
   let totalDailyCookies = 0;
 
   for (let i = 0; i < hours.length; i++) {
-    totalDailyCookies = this.cookiesSoldPerHr[i] + totalDailyCookies;
+    totalDailyCookies = totalDailyCookies + this.cookiesSoldPerHr[i];
   }
-  const totalAllStoresPerHr = document.createElement('td');
-  tableRow.appendChild(totalAllStoresPerHr);
-  totalAllStoresPerHr.textContent = this.totalAllLocations;
+  const total = document.createElement('td');
+  tableRow.appendChild(total);
+  total.textContent = totalDailyCookies;
 };
 
 
+
+function buildSalesTable () {
+  buildHeader();
+//loop over locations and render them
+//all location totals by hour row
+}
+
+function buildHeader() {
+  //this function is going to create a row printing all hours in colums then add row (append)
+}
 
 function totalAllLocations() {
 
@@ -89,21 +81,12 @@ function totalAllLocations() {
   }
 }
 
+let seattle = new Location('Seattle', 23, 65, 6.3);
+let tokyo = new Location('Tokyo', 3, 24, 1.2);
+let dubai = new Location('Dubai', 11, 38, 3.7);
+let paris = new Location('Paris', 20, 38, 3.7);
+let lima = new Location('Lima', 2, 16, 4.6);
 
-let seattle = new Locations('Seattle', 23, 65, 6.3);
-let tokyo = new Locations('Tokyo', 3, 24, 1.2);
-let dubai = new Locations('Dubai', 11, 38, 3.7);
-let paris = new Locations('Paris', 20, 38, 3.7);
-let lima = new Locations('Lima', 2, 16, 4.6);
+const locations = [seattle, tokyo, dubai, paris, lima];
 
-console.log(seattle);
-console.log(tokyo);
-console.log(dubai);
-console.log(paris);
-console.log(lima);
-
-totalAllLocations();
-
-// get functions for hourly cookies and daily location totals into tr/td's
-// get nested functions for getting totals of ALL locations EACH hour combined along bottom
-
+buildSalesTable();
