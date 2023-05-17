42f5275634c7e80d19eedeb0160c7babd0d9bc44
Genevieve DuPuy <genevieve.e.dupuy@gmail.com>
2023-05-16 18:36:58 -0700

    added table background and cleane d up spacing and deleted commented out code. Updated README

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ This a website with information about Pat's Salmon Cookies!
 * 5/11/2023 added sales display function, name and array to location object, css styling and linked index to css, merged class06-objects branch
 * 5/13/2023 created new branch class 07, added constructors for objects and utilized prototypes for functions, attempted creating table with displays unsuccessfully, waiting to merge branch
 * 5/15/23 fixed render function and added table, table data, and header and header function, css styling, and updated README logs and lighthouse png, attempted footer with grand totals but does not work
+* 5/16/23 added table background and cleaned up spacing and commented out code. Updated README.
 
 ### Lighthouse Accessibility Report Score
 
--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 'use strict';
 
-const hours = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
-
+const hours = [
+  '6:00am',
+  '7:00am',
+  '8:00am',
+  '9:00am',
+  '10:00am',
+  '11:00am',
+  '12:00pm',
+  '1:00pm',
+  '2:00pm',
+  '3:00pm',
+  '4:00pm',
+  '5:00pm',
+  '6:00pm',
+  '7:00pm'
+];
 
 function Location(name, minCust, maxCust, avgCookieSale) {
   this.name = name;
@@ -12,23 +26,18 @@ function Location(name, minCust, maxCust, avgCookieSale) {
 }
 
 
-
 Location.prototype.randomCustPerHr = function() {
   return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
 };
 
 
-
 Location.prototype.calculateHourlyCookies = function() {
   for (let i = 0; i < hours.length; i++) {
     let custPerHr = this.randomCustPerHr();
-    console.log(custPerHr);
     let cookiesSold = Math.ceil(custPerHr * this.avgCookieSale);
-    console.log(cookiesSold);
     this.cookiesSoldPerHr.push(cookiesSold);
   }
 };
-
 
 
 const salesTable = document.getElementById('sales-table');
@@ -43,13 +52,13 @@ function buildHeader() {
     row.appendChild(head);
     head.textContent = hours[i];
   }
+
   const total = document.createElement('th');
   row.appendChild(total);
   total.textContent = 'Daily Location Total';
 }
 
 
-//this render function creates a table row, fills it with data, and adds it to the sales table
 Location.prototype.render = function() {
   this.calculateHourlyCookies();
 
@@ -59,23 +68,22 @@ Location.prototype.render = function() {
   tableRow.appendChild(store);
   store.textContent = this.name;
 
+  let totalDailyCookies = 0;
+
   for(let i = 0; i < this.cookiesSoldPerHr.length; i++) {
+    totalDailyCookies = totalDailyCookies + this.cookiesSoldPerHr[i];
+
     const hourlySales = document.createElement('td');
     tableRow.appendChild(hourlySales);
     hourlySales.textContent = this.cookiesSoldPerHr[i];
   }
 
-  let totalDailyCookies = 0;
-  for (let i = 0; i < hours.length; i++) {
-    totalDailyCookies = totalDailyCookies + this.cookiesSoldPerHr[i];
-  }
   const total = document.createElement('td');
   tableRow.appendChild(total);
   total.textContent = totalDailyCookies;
 };
 
 
-//all location totals by hour and Totals written in first cell no worky
 function renderTotals() {
   let grandTotal = 0;
 
@@ -83,39 +91,40 @@ function renderTotals() {
   salesTable.appendChild(row);
   const totalLabel = document.createElement('th');
   row.appendChild(totalLabel);
+  totalLabel.setAttribute('id','total-label');
   totalLabel.textContent = 'Totals';
 
   for(let i = 0; i < hours.length; i++) {
     let hourlyTotal = 0;
+
     for(let j = 0; j < locations.length; j++) {
       hourlyTotal = hourlyTotal + locations[j].cookiesSoldPerHr[i];
       grandTotal = grandTotal + locations[j].cookiesSoldPerHr[i];
     }
+
     const cell = document.createElement('th');
     row.appendChild(cell);
+    cell.setAttribute('id','hourly-total');
     cell.textContent = hourlyTotal;
   }
 
   const foot = document.createElement('th');
   row.appendChild(foot);
+  foot.setAttribute('id','grand-total');
   foot.textContent = grandTotal;
 }
-// totals();
 
 
-
-let seattle = new Location('Seattle', 23, 65, 6.3);
-let tokyo = new Location('Tokyo', 3, 24, 1.2);
-let dubai = new Location('Dubai', 11, 38, 3.7);
-let paris = new Location('Paris', 20, 38, 3.7);
-let lima = new Location('Lima', 2, 16, 4.6);
-
-
-const locations = [seattle, tokyo, dubai, paris, lima];
+const locations = [
+  new Location('Seattle', 23, 65, 6.3),
+  new Location('Tokyo', 3, 24, 1.2),
+  new Location('Dubai', 11, 38, 3.7),
+  new Location('Paris', 20, 38, 3.7),
+  new Location('Lima', 2, 16, 4.6),
+];
 
 buildHeader();
 for(let i = 0; i < locations.length; i++) {
   locations[i].render();
 }
-
 renderTotals();
